c811cec3d7edde85d0a9609d56326a3dfbe35850
june38 <sutattajune@hotmail.com>
2014-12-02 15:44:20 +0700

    final commit

--- lib/lovemac.rb
+++ lib/lovemac.rb
@@ -1,3 +1,22 @@
-def lovemac(number)
-  # Put your code in here  
+def lovemac(input)
+	a =0
+	b =0
+	c =0
+	for i in 1..input
+		if i % 3 == 0
+			if i % 5 == 0
+				puts "HateWindows"
+				c = c + 1
+			else
+				puts "Love"
+				a= a + 1
+			end
+		elsif i % 5 == 0
+ 			puts "Mac"
+ 			b = b + 1
+		else
+   			puts "#{i}" 
+   		end
+	end
+	return "("+a.to_s+","+b.to_s+","+c.to_s+")"
 end
--- spec/lib/lovemac_spec.rb
+++ spec/lib/lovemac_spec.rb
@@ -1,5 +1,16 @@
-require "./lovemac.rb"
+require "lovemac"
+require "spec_helper"
 
-describe "lovemac" do
-  # Write your test cases in here
+describe "#lovemac(number)" do
+
+	it "Test if print how many number of word of Love, Mac, HateWindows are printed" do
+		#Describe method scenario
+		status = lovemac(10);
+		expect(status).to eq("(3,2,0)")
+	end
+	it "Test if print how many number of word of Love, Mac, HateWindows are printed" do
+		#Describe method scenario
+		status = lovemac(50);
+		expect(status).to eq("(13,7,3)")
+	end
 end
